ec0f1166933b87e8944db701130c21adf5929cb5
Shogun89 <ryhowe@gmail.com>
2019-07-11 21:11:23 -0400

    another one

--- turn_to_csv.py
+++
@@ -1,8 +0,0 @@
-
-
-filename = "C:\\Users\\Ryan Howe\\Documents\\Python\\Shower\\clean_shower.txt"
-
-csvfile = "clean_shower.csv"
-import pandas as pd
-df = pd.read_fwf(filename)
-df.to_csv(csvfile)
